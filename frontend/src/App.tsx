447b69a84f6bc27601ddd233cec5954fd861bf26
Viktor Zhokhov <viktor.zhokhov@fundraiseup.com>
2026-08-15 17:25:08 +0200

    Задание 1.2: замена Code Grant на PKCE (Keycloak + фронтенд)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,13 @@ const keycloak = new Keycloak(keycloakConfig);
 
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider
+      authClient={keycloak}
+      initOptions={{
+        pkceMethod: 'S256',
+        onLoad: 'check-sso',
+      }}
+    >
       <div className="App">
         <ReportPage />
       </div>
